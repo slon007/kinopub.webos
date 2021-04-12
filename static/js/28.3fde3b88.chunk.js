ecd25f78de7e503057b8fcc1a2ba68e1d6f890dd
adascal <adascal@users.noreply.github.com>
2021-04-12 19:35:07 +0000

    deploy: b960c9c1fc6bf2a41d21346f5f4c5327428d5771

---
+++ static/js/28.3fde3b88.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkinopub.webos"]=this["webpackJsonpkinopub.webos"]||[]).push([[28],{729:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return u}));var o=e(1),u=function(){return Object(o.jsx)(o.Fragment,{children:"Not found"})}}}]);
+//# sourceMappingURL=28.3fde3b88.chunk.js.map
